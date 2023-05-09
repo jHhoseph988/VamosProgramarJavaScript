0a75487382e5279de1e83ccfba67f68816500018
jHhoseph988 <131305638+jHhoseph988@users.noreply.github.com>
2023-05-09 11:26:16 +0200

    ejemplos  de variables

--- variables.js
+++ variables.js
@@ -1,1 +1,15 @@
-//principios de java escript
+var numero1; 
+var numero2= 9;
+cosole.log(a);
+console.log(b);
+
+var numero3 = 4;
+var numero4 = 6;
+cosole.log(a);
+console.log(b);
+
+var numero5 = 7;
+b = numero5;
+
+cosole.log(a);
+console.log(b);
